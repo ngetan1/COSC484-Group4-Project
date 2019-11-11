6e27356dc6db03fa19d2eb3fc683aa3caf9cbe7b
ngetan1 <43097300+ngetan1@users.noreply.github.com>
2019-11-11 14:46:59 -0500

    Allow express to use images

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ var express = require("express");
 var app = express();
 
 app.use("/css", express.static("./css"));
-app.use("/js", express.static("./js"))
+app.use("/js", express.static("./js"));
+app.use("/img", express.static("./img"));
 
 app.get("/session", (req, res)=>{
     res.sendFile(__dirname+"/session.html");
