49dd8e89a209fc2681159d9051f88e62362a1099
ChrisCox416 <55766145+ChrisCox416@users.noreply.github.com>
2019-11-12 15:37:07 -0500

    Update app.js

--- app.js
+++ app.js
@@ -14,4 +14,13 @@ app.get("/login", (req, res)=>{
     res.sendFile(__dirname+"/login.html");
 
 });
+
+app.get("/create_account", (req, res)=>{
+    res.sendFile(__dirname + "/create_account.html");
+});
+
+app.get("/create_account_redirect", (req, res)=>{
+    res.sendFile(__dirname + '/create_account_redirect.html');
+});
+
 app.listen(3000);
