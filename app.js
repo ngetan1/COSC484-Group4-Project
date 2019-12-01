6f9a34ace91b0801b5bfaa95aeffc596c1eb83b7
ChrisCox416 <55766145+ChrisCox416@users.noreply.github.com>
2019-11-30 21:46:12 -0500

    Updated create_account and app.js, Added create-account.css
    Updated create_account.html so that it is more presentable and added css for it. Added some mysql querying for when someone creates an account (Need to fill it in with credentials for the database).

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var app = express();
+var mysql= require("mysql");
 
 app.use("/css", express.static("./css"));
 app.use("/js", express.static("./js"));
@@ -27,12 +28,47 @@ app.get("/search", (req, res)=>{
 
 app.get("/createAccount", (req, res)=>{
     res.sendFile(__dirname+"/create_account.html");
-
+    
 });
 
 app.get("/createAccountRedirect", (req, res)=>{
     res.sendFile(__dirname+"/postlogin.html");
     
+    var parsedURL = url.parse(req.url, true);
+    var fname= req.query.fname;
+    var lname = req.query.lname;
+    var uid = req.query.uid;
+    var email = req.query.email;
+    var pw = req.query.pw;
+    var isteach = req.query.isteach;
+    if(isteach == "Yes"){
+        isteach = 1;
+    }
+    else{
+        isteach = 0;
+    }
+
+    var con = mysql.createConnection({
+        host: "...",
+        user: "...",
+        password: "...",
+        database: "mydb"
+    });
+
+    con.connect(function(err){
+        if (err) throw err;
+        console.log("Connected");
+        
+        var sql = "INSERT INTO account (UserID, password, FName, LName, Email, IsTeacher) VALUES ?";
+        var values = [
+            [uid, pw, fname, lname, email, isteach]
+        ];
+
+        con.query(sql, [values], function(err, result){
+            if (err) throw err;
+            console.log('account added');
+        });
+    });
 });
 
 app.get("/aboutUs", (req, res)=>{
